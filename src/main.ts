e8f147f4ebee02990f7001f58904d2cd6c819123
Daniel martínez <dmartinezh97@gmail.com>
2023-09-25 18:43:41 +0200

    Add mount

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import { HelloWorld } from '@/components/main.ts'
+import App from '@/App.vue'
 
-const app = createApp({})
+const app = createApp(App)
 
 app
     .component('HelloWorld', HelloWorld)
+
+app.mount('#app')
